5fe97ef3c18c157f213de0a8778a5ff31befada9
muyuca <muhammedyusufcakir1@gmail.com>
2024-04-29 12:19:33 +0300

    patika

--- Boks_Maci/Fighter.java
+++ Boks_Maci/Fighter.java
@@ -17,7 +17,7 @@ public class Fighter {
         System.out.println(this.name + " Boksörü " + enemy.name+" Boksörüne "+this.damage+ " hasar vurdu .");
         double random = Math.random()*100;
         if (enemy.dodge > random ){
-            System.out.println("---------------"+enemy.name +" Boksörü vuruşu dodgladı ! -------------");
+            System.out.println("------------"+enemy.name +" Boksörü vuruşu dodgladı ! -----------");
             return enemy.health;
         }
         if(enemy.health-this.damage >= 0){
